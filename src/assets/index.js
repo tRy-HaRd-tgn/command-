93d6d800f88c9effb3f77b869fe670f6e743538e
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-12-04 17:24:59 +0300

    feat:add nav arrows fix:fix some styles

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,5 @@ import phone from "./phone.svg";
 import img from "./main_start_img.png";
 import square from "./square.svg";
 import arrowRight from "./arrowRight.svg";
-export { square, brain, phone, img, arrowRight };
+import arrowLeft from "./arrowLeft.svg";
+export { square, brain, phone, img, arrowRight, arrowLeft };
--- src/pages/profile-page2/index.jsx
+++ src/pages/profile-page2/index.jsx
@@ -1,12 +1,13 @@
 import styles from "./styles.module.scss";
-import { Header } from "../../components";
-import { Input } from "../../components";
-import { Button } from "../../components";
+import { Header, Button, Input } from "../../components";
+import { arrowRight, arrowLeft } from "../../assets";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export const ProfilePage2 = (props) => {
   const [group, setGroup] = useState("");
   const [direction, setDirection] = useState("");
   const [project, setProject] = useState("");
+  const navigator = useNavigate();
   return (
     <>
       <div className={styles.container}>
@@ -16,69 +17,81 @@ export const ProfilePage2 = (props) => {
         <div className={styles.container__content}>
           <div className={styles.container__content__info}>
             <h2 className={styles.h2}>Профиль</h2>
-            <div className={styles.container__content__info__flex}>
-              <div>
-                <Input
-                  style={{
-                    height: "29%",
-                    fontSize: "135%",
-                  }}
-                  placeholder={"должность/академическая группа"}
-                  type="text"
-                  value={group}
-                  onChange={(e) => setGroup(e.target.value)}
-                />
-                <Input
-                  style={{
-                    height: "29%",
-                    fontSize: "135%",
-                  }}
-                  placeholder={"направление обучения"}
-                  type="text"
-                  value={direction}
-                  onChange={(e) => setDirection(e.target.value)}
-                />
-              </div>
-              <div className={styles.buttons_wrapper}>
-                <Button className={styles.form_button}>
-                  оценка Softskills
-                </Button>
+            <div className={styles.divNav}>
+              <img
+                onClick={() => {
+                  navigator("/");
+                }}
+                className={styles.arrow}
+                src={arrowLeft}
+                alt=""
+              />
+              <div className={styles.container__content__info__flex}>
+                <div className={styles.container__content__info__flex__inputs}>
+                  <Input
+                    style={{
+                      height: "29%",
+                      fontSize: "135%",
+                    }}
+                    placeholder={"должность/академическая группа"}
+                    type="text"
+                    value={group}
+                    onChange={(e) => setGroup(e.target.value)}
+                  />
+                  <Input
+                    style={{
+                      height: "29%",
+                      fontSize: "135%",
+                    }}
+                    placeholder={"направление обучения"}
+                    type="text"
+                    value={direction}
+                    onChange={(e) => setDirection(e.target.value)}
+                  />
+                </div>
+                <div className={styles.buttons_wrapper}>
+                  <Button className={styles.form_button}>
+                    оценка Softskills
+                  </Button>
+                  <Button
+                    style={{
+                      backgroundColor: "grey",
+                      color: "white",
+                      borderColor: "grey",
+                    }}
+                    className={styles.form_button}
+                  >
+                    оценка Hardskills
+                  </Button>
+                  <Input
+                    style={{
+                      height: "100%",
+                      margin: "0",
+                      paddingTop: "1.1%",
+                      paddingBottom: "1.1%",
+                      paddingRight: "0.65%",
+                      fontSize: "135%",
+                    }}
+                    placeholder={"предложить проект"}
+                    type="text"
+                    value={project}
+                    onChange={(e) => setProject(e.target.value)}
+                  />
+                </div>
                 <Button
                   style={{
-                    backgroundColor: "grey",
-                    color: "white",
-                    borderColor: "grey",
+                    height: "7%",
+                    width: "24%",
+                    textAlign: "center",
+                    fontSize: "calc(125% / 1.2)",
+                    marginLeft: "8%",
                   }}
                   className={styles.form_button}
                 >
-                  оценка Hardskills
+                  сохранить
                 </Button>
-                <Input
-                  style={{
-                    height: "100%",
-                    margin: "0",
-                    paddingTop: "1.1%",
-                    paddingBottom: "1.1%",
-                    paddingRight: "0.65%",
-                    fontSize: "135%",
-                  }}
-                  placeholder={"предложить проект"}
-                  type="text"
-                  value={project}
-                  onChange={(e) => setProject(e.target.value)}
-                />
               </div>
-              <Button
-                style={{
-                  height: "7%",
-                  width: "24%",
-                  textAlign: "center",
-                  fontSize: "calc(125% / 1.2)",
-                }}
-                className={styles.form_button}
-              >
-                сохранить
-              </Button>
+              <img className={styles.arrow} src={arrowRight} alt="" />
             </div>
           </div>
         </div>
