79d944c703101ce4d39df8920d43560d1f7704d7
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-03-27 00:24:13 +0300

    feat: delete aiService

--- src/service/AiService.js
+++
@@ -1,6 +0,0 @@
-import $api from "../http";
-export default class AiService {
-  static async ollamaTest(prompt, model, stream) {
-    return $api.post("api/generate", { prompt, model, stream });
-  }
-}
