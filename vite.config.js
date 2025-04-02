8b03c03c01a86f1ae9d5782c3b3ad172f23e2003
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-04-03 01:11:50 +0300

    feat: update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler", // or "modern"
-      },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ["last 2 versions"],
+          grid: true,
+        }),
+        cssnano({
+          preset: [
+            "default",
+            {
+              autoprefixer: false, // Оставляем префиксы нетронутыми
+              discardUnused: false, // Не удаляем неиспользуемые стили
+              reduceIdents: false, // Не сокращаем идентификаторы
+            },
+          ],
+        }),
+      ],
     },
   },
   crossDomain: true,
