4296045f40a03e3ad51e3c841e54e40733219007
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-04-02 13:36:16 +0300

    delete putin image feat: new logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,6 @@ import img from "./main_start_img.png";
 import square from "./square.svg";
 import arrowRight from "./arrowRight.svg";
 import arrowLeft from "./arrowLeft.svg";
-import putin from "./putin.jpg";
 import download from "./download.svg";
 import phone2 from "./phone.png";
 import telegram from "./telegram.png";
@@ -16,7 +15,6 @@ export {
   img,
   arrowRight,
   arrowLeft,
-  putin,
   phone2,
   telegram,
 };
