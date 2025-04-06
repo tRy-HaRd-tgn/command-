4c965932a522dbbd3b260da1f003697c7b533a2f
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-04-06 18:51:05 +0300

    feat: add default profile photo for project on moderation and in progress

--- src/components/projectComponent/index.jsx
+++ src/components/projectComponent/index.jsx
@@ -1,8 +1,9 @@
 import styles from "./styles.module.scss";
+import profile from "../../assets/profile.png";
 export const ProjectComp = ({ name, description, img, term }) => {
   return (
     <div className={styles.wrapper}>
-      <img className={styles.img} src={img} alt="всё хорошо" />
+      <img className={styles.img} src={img ? img : profile} alt="всё хорошо" />
       <p>{name}</p>
       <p>{term}</p>
       <p>{description}</p>
